2003d86d6e8031f28faace8f548324940716f367
pwang53 <306503537@qq.com>
2020-03-05 22:26:35 -0600

    Display static page

--- Inst/settings.py
+++ Inst/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Inst.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
