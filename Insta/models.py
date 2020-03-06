1f3065660a1e8aa5a6411a90247c0f5e58903aca
pwang53 <306503537@qq.com>
2020-03-06 00:31:34 -0600

    Define Model & Update Database & Set Admin

--- Insta/models.py
+++ Insta/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from imagekit.models import ProcessedImageField  # 第三方库：django-image
+
 
 # Create your models here.
+# 有不会的就谷歌Django官方文档: google Django Model Fields
+# https://docs.djangoproject.com/en/3.0/ref/models/fields/
+
+# 定义一个model
+class Post(models.Model):
+    title = models.TextField(blank=True, null=True)
+    image = ProcessedImageField(
+        # 当有人上传一个图片时，存到服务器哪个路径
+        upload_to='static/images/posts',
+        # 希望图片格式是
+        format='JPEG',
+        # 图片质量为100
+        options={'quality': 100},
+        # 允许图片是空白的
+        blank=True,
+        # 允许null图片存在
+        null=True
+    )
+
